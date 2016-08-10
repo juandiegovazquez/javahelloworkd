b7e172049cd43fbbcf45c85a78fa58554101776f
Vazquez Fuentesal <j.vazquez.fuentesal@accenture.com>
2016-08-10 15:07:24 +0200

    test change

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,5 +3,6 @@ public class JavaHelloWorld
 	public static void main(String[] args){
 		System.out.println("Hello world");
 		System.out.println("Hola mundo");
+		System.out.println("Hola mundo otra vez");
 	}
 }
